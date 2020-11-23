d6a5c2c051dba5af218188338440e65f9ffcae14
Joseph Lee <jin3lee@gmail.com>
2020-11-23 13:16:18 -0600

    add more dependancies

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyDsBaoDbYMjmBSbN_e1448CSRu_z1a5j90",
+    authDomain: "apployme-be1b6.firebaseapp.com",
+    databaseURL: "https://apployme-be1b6.firebaseio.com",
+    projectId: "apployme-be1b6",
+    storageBucket: "apployme-be1b6.appspot.com",
+    messagingSenderId: "433096878896",
+    appId: "1:433096878896:web:e3da8a2a7087c8949f76f5"
+  }
+}
